d71ef5fc6b3919f94f7857e97e073c5745ee754e
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-05-11 21:42:33 -0300

    Add files via upload

---
+++ projeto02-login_form/script.js
@@ -0,0 +1,54 @@
+/*
+# Autor: Arreaum
+# GitHub: https://github.com/arriaoedu123/
+# Data de criação: 05/11/2020
+# Data de atualização: 11/05/2020
+# Versão: 1.1
+*/
+
+$(".options-04 a").click(function () {
+        $('.registro-form').hide("slow");
+        $('.recuperacao-form').hide("slow");
+        $(".login-form").animate(
+          {
+            height: "toggle",
+            opacity: "toggle",
+          },
+          "slow"
+        );
+      });
+
+      $(".options-03 a").click(function () {
+        $('.registro-form').hide("slow");
+        $('.recuperacao-form').hide("slow");
+        $(".login-form").animate(
+          {
+            height: "toggle",
+            opacity: "toggle",
+          },
+          "slow"
+        );
+      });
+
+      $(".options-02 a").click(function () {
+        $('.login-form').hide("slow");
+        $('.recuperacao-form').hide("slow");
+        $(".registro-form").animate(
+          {
+            height: "toggle",
+            opacity: "toggle",
+          },
+          "slow"
+        );
+      });
+
+      $(".options-01 a").click(function () {
+        $('.login-form').hide("slow");
+        $(".recuperacao-form").animate(
+          {
+            height: "toggle",
+            opacity: "toggle",
+          },
+          "slow"
+        );
+      });
