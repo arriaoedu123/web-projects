beab23317ea02a3082a3b9a4c88ab0e25d786eb6
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-06-27 14:46:42 -0300

    Add files via upload

---
+++ project07-countdown_timer/script.js
@@ -0,0 +1,31 @@
+/*
+# Creator: Arreaum
+# GitHub: https://github.com/arriaoedu123/
+# Creation date: 27/06/2021
+# Version: 1.0
+*/
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`December 25 ${currentYear} 00:00:00`);
+
+// Countdown timer update
+function updateCountdown() {
+	const currentTime = new Date();
+	const diff = newYearTime - currentTime;
+
+	const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+	const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+	const m = Math.floor(diff / 1000 / 60) % 60;
+	const s = Math.floor(diff / 1000) % 60;
+
+	days.innerHTML = d;
+	hours.innerHTML = h < 10 ? '0' + h : h;
+	minutes.innerHTML = m < 10 ? '0' + m : m;
+	seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
