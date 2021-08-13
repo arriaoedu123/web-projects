4a0fbab23bb59bc41d759ba864936d9924b67e36
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-08-13 16:45:56 -0300

    Fixing bugs

--- project09-neumorphism_design/script.js
+++ project09-neumorphism_design/script.js
@@ -2,19 +2,19 @@
 # Creator: Arreaum
 # GitHub: https://github.com/arriaoedu123/
 # Creation date: 10/08/2021
-# Version: 1.0
+# Update date: 13/08/2021
+# Version: 1.1
 */
 
 const body = document.querySelector('body');
 
+textInput.value = ""; //the input value will no longer remain the same when you reload the page
+
 if (!body.classList.contains('dark')) {
-	isCheck.checked = false;
+	isCheck.checked = false; //the checkbox will no longer remain checked when you reload the page
 }
 
 function toggleMode() {
 	body.classList.toggle('dark');
-}
-
-document.querySelector('.toggleMode').addEventListener('click', function(){
 	document.querySelector('.checkText').classList.toggle('checkMode');
-});
+}
