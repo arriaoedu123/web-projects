8220df054844d7b8fe86d7c3c8ac4031b4086e1c
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-08-19 16:23:35 -0300

    Improving the style

--- project02-login_form/script.js
+++ project02-login_form/script.js
@@ -2,53 +2,58 @@
 # Creator: Arreaum
 # GitHub: https://github.com/arriaoedu123/
 # Creation date: 05/11/2020
-# Update date: 11/05/2020
-# Version: 1.1
+# Update date: 19/08/2021
+# Version: 1.2
 */
 
+//coming back to sign in form when click on "Sign in" in recovery form
 $(".options-04 a").click(function () {
-        $('.registro-form').hide("slow");
-        $('.recuperacao-form').hide("slow");
-        $(".login-form").animate(
-          {
-            height: "toggle",
-            opacity: "toggle",
-          },
-          "slow"
-        );
-      });
+  $('.signup-form').hide("slow");
+  $('.recovery-form').hide("slow");
+  $(".login-form").animate(
+  {
+    height: "toggle",
+    opacity: "toggle",
+  },
+  "slow"
+  );
+});
 
-      $(".options-03 a").click(function () {
-        $('.registro-form').hide("slow");
-        $('.recuperacao-form').hide("slow");
-        $(".login-form").animate(
-          {
-            height: "toggle",
-            opacity: "toggle",
-          },
-          "slow"
-        );
-      });
 
-      $(".options-02 a").click(function () {
-        $('.login-form').hide("slow");
-        $('.recuperacao-form').hide("slow");
-        $(".registro-form").animate(
-          {
-            height: "toggle",
-            opacity: "toggle",
-          },
-          "slow"
-        );
-      });
+//coming back to sign in form when click on "Sign in" in sign up form
+$(".options-03 a").click(function () {
+  $('.signup-form').hide("slow");
+  $('.recovery-form').hide("slow");
+  $(".login-form").animate(
+  {
+    height: "toggle",
+    opacity: "toggle",
+  },
+  "slow"
+  );
+});
 
-      $(".options-01 a").click(function () {
-        $('.login-form').hide("slow");
-        $(".recuperacao-form").animate(
-          {
-            height: "toggle",
-            opacity: "toggle",
-          },
-          "slow"
-        );
-      });
+//going to sign up form when click on "Sign up" in sign in form
+$(".options-02 a").click(function () {
+  $('.login-form').hide("slow");
+  $('.recovery-form').hide("slow");
+  $(".signup-form").animate(
+  {
+    height: "toggle",
+    opacity: "toggle",
+  },
+  "slow"
+  );
+});
+
+//going to recovery form when click on "Forgot passowrd?" in sign in form
+$(".options-01 a").click(function () {
+  $('.login-form').hide("slow");
+  $(".recovery-form").animate(
+  {
+    height: "toggle",
+    opacity: "toggle",
+  },
+  "slow"
+  );
+});
