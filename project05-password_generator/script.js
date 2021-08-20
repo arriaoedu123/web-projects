3e9c49330cb7ec020179df7f2c5ffe28c8dc54a5
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-08-20 00:46:36 -0300

    Update files

--- project05-password_generator/script.js
+++ project05-password_generator/script.js
@@ -2,16 +2,19 @@
 # Creator: Arreaum
 # GitHub: https://github.com/arriaoedu123/
 # Creation date: 14/05/2021
-# Update date: 15/05/2021
-# Version: 1.1
+# Update date: 19/08/2021
+# Version: 1.2
 */
 
-// Generate Password function
+document.getElementById("password").value = ""; //starting with empty password field
+
+//function to generate password
 function getPassword() {
-	var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&{}()";
-	var passwordLength = 16;
+	var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&{}()"; //this is every password characters
+	var passwordLength = 16; //max password length
 	var password = "";
 
+	//choosing a random character to generate the password
 	for (var i=0; i<passwordLength; i++) {
 		var randomNumber = Math.floor(Math.random() * chars.length);
 		password += chars.substring(randomNumber, randomNumber+1);
@@ -19,7 +22,7 @@ function getPassword() {
 	document.getElementById("password").value = password;
 }
 
-// Copy Password function
+//function to copy password
 function copyPassword() {
 	var copyPass = document.getElementById("password");
 	copyPass.select();
